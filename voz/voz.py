7994d42886f3b760f924c1c265aaa9d4e7619174
Juan Castro Fernández <castrofernandez@gmail.com>
2014-01-26 20:17:01 +0100

    Voz

--- voz/voz.py
+++ voz/voz.py
@@ -2,6 +2,9 @@ from subprocess import call
 from time import sleep
 import datetime
 
+INICIO = 8
+FIN = 21
+
 def hablar(texto):
     print texto
     url = "http://translate.google.com/translate_tts?tl=es&q=" + texto
@@ -15,10 +18,42 @@ def esperar(tiempo):
    segundos = tiempo % 60
    print "Esperando " + str(minutos) + " minutos y " + str(segundos) + " segundos"
    sleep(tiempo)
+   
+def hora(h):
+    if h > 12:
+        h -= 12
+    if h == 1:
+        hablar("Es la una")
+    elif h == 2:
+        hablar("Son las dos")
+    elif h == 3:
+        hablar("Son las tres")
+    elif h == 4:
+        hablar("Son las cuatro")
+    elif h == 5:
+        hablar("Son las cinco")
+    elif h == 6:
+        hablar("Son las seis")
+    elif h == 7:
+        hablar("Son las siete")
+    elif h == 8:
+        hablar("Son las ocho")
+    elif h == 9:
+        hablar("Son las nueve")
+    elif h == 10:
+        hablar("Son las diez")
+    elif h == 11:
+        hablar("Son las once")
+    else:
+        hablar("Son las doce")
      
 hablar("hola")
 ahora = datetime.datetime.now().time()
 # Esperamos a la siguiente hora
 espera = (60 - ahora.minute) * 60 - ahora.second
 esperar(espera)
-hablar("es la hora")
+while(True):
+    ahora = datetime.datetime.now().time()
+    if ahora.hour >= INICIO and ahora.hour <= FIN:
+        hora(ahora.hour)
+    esperar(3600) # Se espera a la siguiente hora
