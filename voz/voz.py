cb0de086675217a4c90ec9ac89ce083de75bed5c
Juan Castro Fernández <castrofernandez@gmail.com>
2014-01-26 19:58:01 +0100

    Espera

--- voz/voz.py
+++ voz/voz.py
@@ -5,9 +5,20 @@ import datetime
 def hablar(texto):
     print texto
     url = "http://translate.google.com/translate_tts?tl=es&q=" + texto
-    call(["mpg123", "-q", url])
+    try:
+        call(["mpg123", "-q", url])
+    except:
+        pass
+        
+def esperar(tiempo):
+   minutos = tiempo / 60
+   segundos = tiempo % 60
+   print "Esperando " + str(minutos) + " minutos y " + str(segundos) + " segundos"
+   sleep(tiempo)
      
 hablar("hola")
-print datetime.datetime.now().time()
-sleep(60)
-print datetime.datetime.now().time()
+ahora = datetime.datetime.now().time()
+# Esperamos a la siguiente hora
+espera = (60 - ahora.minute) * 60 - ahora.second
+esperar(espera)
+hablar("es la hora")
