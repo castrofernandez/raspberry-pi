89b1ceaf9352724ead1f83817a1406b0c1716d67
Juan Castro Fernández <castrofernandez@gmail.com>
2014-01-26 19:30:42 +0100

    Error

--- voz/voz.py
+++ voz/voz.py
@@ -2,6 +2,7 @@ from subprocess import call
 
 def hablar(texto):
     print texto
-    call(["mpg123", '-q "http://translate.google.com/translate_tts?tl=es&q=' + texto + '"'])
+    url = "http://translate.google.com/translate_tts?tl=es&q=" + texto
+    call(["mpg123", "-q", url])
      
 hablar("hola")
