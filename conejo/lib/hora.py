96d3fadcaf8673e3cc573e0f2633e57a6f2c1f4f
Juan Castro Fernández <castrofernandez@gmail.com>
2014-06-11 21:42:08 +0200

    Print

--- conejo/lib/hora.py
+++ conejo/lib/hora.py
@@ -25,7 +25,7 @@ def obtenerTiempo():
 def decirHora(fichero):
     obtenerMP3('ES', obtenerTiempo(), fichero)
     
-    return "PLAY %s" % fichero
+    print "PLAY %s" % fichero
 
 if __name__=="__main__":
     decirHora('./audio/hora.mp3')
