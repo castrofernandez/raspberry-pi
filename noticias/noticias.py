3a5e7d8757b7672976a3de9fd299fcc3e2068f91
Juan Castro Fernández <castrofernandez@gmail.com>
2014-06-11 11:12:25 +0200

    fuentes

--- noticias/noticias.py
+++ noticias/noticias.py
@@ -16,24 +16,47 @@ class Noticias:
     def insertar(self, titulo, resumen, enlace):
         self.noticias.append((titulo, resumen, enlace))
 
+    def obtener(self, indice):
+        return self.noticias[indice]
+
+    def longitud(self):
+        return len(self.noticias)
+
 def obtenerNoticias():
     # Retorno
-    retorno = Noticias()
+    todas_noticias = []
 
     # Procesamos fuentes
     for fuente in fuentes:
+        resultado = Noticias()
+        todas_noticias.append(resultado)
         # Descargamos la fuente
         noticias = feedparser.parse(fuente)
         titulo_fuente = noticias['feed']['title']
+
         # Recorremos noticias
         for noticia in noticias['entries']:
             noticia_titulo =  noticia['title'].encode('UTF-8', 'replace')
             noticia_resumen = nltk.clean_html(noticia['summary']).encode('UTF-8', 'replace')
             noticia_enlace = noticia['link'].encode('UTF-8', 'replace')
 
-            retorno.insertar(noticia_titulo, noticia_resumen, noticia_enlace)
+            resultado.insertar(noticia_titulo, noticia_resumen, noticia_enlace)
 
-    return retorno
+    resultado = Noticias()
+
+    longitud = 0
+
+    for fuente in todas_noticias:
+      longitud = max(longitud, fuente.longitud())
+
+    for i in range(longitud):
+      for fuente in todas_noticias:
+        if (i >= fuente.longitud()):
+          continue
+
+        resultado.insertar(fuente.obtener(i)[0], fuente.obtener(i)[1], fuente.obtener(i)[2])
+
+    return resultado
 
 if __name__ == "__main__":
     print obtenerNoticias()
