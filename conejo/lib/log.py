bac622a5d3c7496fe9ad38b1121adad77fb3aabd
Juan Castro Fernández <castrofernandez@gmail.com>
2014-06-12 09:46:00 +0200

    locale

--- conejo/lib/log.py
+++ conejo/lib/log.py
@@ -4,7 +4,7 @@ import time
 import locale
 
 def auditarPeticion(fichero, serie, boton, rfid):
-  locale.setlocale(locale.LC_TIME, "es_ES")
+  #locale.setlocale(locale.LC_TIME, "es_ES")
   hora = time.strftime("%a, %d %b %Y %H:%M:%S")
 
   linea = "%s - serie: %s botón: %s RFID: %s \n" % (hora, serie, boton, rfid)
