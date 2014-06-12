e9c63626017500424bff541d4b83628100b61d9f
Juan Castro Fernández <castrofernandez@gmail.com>
2014-06-12 10:58:07 +0200

    debug

--- conejo/lib/comando.py
+++ conejo/lib/comando.py
@@ -5,12 +5,15 @@ from hora import decirHora
 from tts import obtenerMP3
 
 def procesarComando(serie, boton, rfid):
-  if boton == "3":
-    return procesarAccionBoton()
-  elif rfid != None and rfid != "":
-    return procesarRFID(rfid)
-  else:
-    return ejecutarAccionProgramada()
+  #if boton == "3":
+  #  return procesarAccionBoton()
+  #elif rfid != None and rfid != "":
+  #  return procesarRFID(rfid)
+  #else:
+  #  return ejecutarAccionProgramada()
+  boton = "si" if boton == "3" else "no"
+  nariz = "si" if rfid != None and rfid != "" else "no"
+  return obtenerMP3('ES', "Boton %s y Nariz %s" % (boton, nariz), './audio/aux.mp3')
 
 def procesarAccionBoton():
   return obtenerMP3('ES', "Me has tocado la cabeza", './audio/boton.mp3')
